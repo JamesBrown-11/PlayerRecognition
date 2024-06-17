11ab54ef36d801354aa1b9c7cc6be218c53b3e35
dr-james-brown <111708026+dr-james-brown@users.noreply.github.com>
2024-06-17 13:55:01 -0400

    Updated code

--- train.py
+++ train.py
@@ -3,4 +3,6 @@ import os
 
 model = YOLO('yolov9c.yaml')
 
-results = model.train(data='data\\data.yaml', epochs=100, imgsz=640)
+results = model.train(data='datasets\\data.yaml', epochs=100, imgsz=640)
+
+print(results)

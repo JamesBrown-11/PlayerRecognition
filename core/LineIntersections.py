b4f9383d60014a3828ae82160773cf660507c95d
jlbrown5-ncat <60116362+jlbrown5-ncat@users.noreply.github.com>
2025-08-26 12:36:09 -0400

    Cleanup

--- core/LineIntersections.py
+++ core/LineIntersections.py
@@ -22,59 +22,10 @@ class Point:
     def displayPoint(self, p):
         print(f"({p.x}, {p.y})")
 
-class LineIdentification:
+class LineExtractor:
     def __init__(self):
         self.img = None
         self.pixels = None
-
-
-    def extract_pixels(self, img):
-        self.img = Image.open(img)
-        self.pixels = self.img.load()
-
-
-    def calculate_pixel_luminance(self, r, g, b):
-        return (r * 0.2126) + (g * 0.7152) + (b * 0.0722)
-
-    def find_lines(self):
-        size = self.img.size
-
-        for x in range(size[0]):
-            for y in range(size[1]):
-                current_pixel = self.pixels[x,y]
-                pixel_r = self.pixels[x, y][0]
-                pixel_g = self.pixels[x, y][1]
-                pixel_b = self.pixels[x, y][2]
-                pixel_luminance = self.calculate_pixel_luminance(pixel_r, pixel_g, pixel_b)
-                # check if the rgb values of the pixel are white
-                # each value must be greater tha 240
-                if pixel_luminance >= SIGMA_L:
-                    # Since this is considered a white pixel, determine if the pixels LINE_WIDTH linearly
-                    # Must account for if the current pixel is near the left edge
-                    left_most_x = max(x - LINE_WIDTH, 0)
-                    right_most_x = min(x + LINE_WIDTH, size[0]-1)
-                    top_most_y = max(y - LINE_WIDTH, 0)
-                    bottom_most_y = min(y + LINE_WIDTH, size[1]-1)
-
-                    left_most_pixel = self.pixels[left_most_x, y]
-                    right_most_pixel = self.pixels[right_most_x, y]
-                    top_most_pixel = self.pixels[x, top_most_y]
-                    bottom_most_pixel = self.pixels[x, bottom_most_y]
-
-                    left_most_pixel_luminance = self.calculate_pixel_luminance(left_most_pixel[0], left_most_pixel[1], left_most_pixel[2])
-                    right_most_pixel_luminance = self.calculate_pixel_luminance(right_most_pixel[0], right_most_pixel[1], right_most_pixel[2])
-                    top_most_pixel_luminance = self.calculate_pixel_luminance(top_most_pixel[0], top_most_pixel[1], top_most_pixel[2])
-                    bottom_most_pixel_luminance = self.calculate_pixel_luminance(bottom_most_pixel[0], bottom_most_pixel[1], bottom_most_pixel[2])
-
-                    if ((pixel_luminance - left_most_pixel_luminance >= SIGMA_D and pixel_luminance - right_most_pixel_luminance >= SIGMA_D)
-                            or (pixel_luminance - top_most_pixel_luminance >= SIGMA_D and pixel_luminance - bottom_most_pixel_luminance >= SIGMA_D)):
-                        # print('White line detection')
-                        # If this check is passed, the candidate pixel is considered on a line
-                        # TODO: We must exclude white pixels that are in textrued regions i.e. letters in logos, white areas in stadium, or spectators
-                        # TODO: Hough Line Detection
-
-                        self.img.putpixel((x, y), (0, 0, 0))
-        # self.img.show()
 
     def edge_detection(self, frame):
         size = frame.shape
@@ -91,6 +42,50 @@ class LineIdentification:
         # cv2.waitKey(0)
 
         return edges
+
+    def calculate_orientation(self, edges):
+        # Apply HoughLinesP method to directly obtain line end points
+        lines_list = []
+        quadratics_list = []
+        lines = cv2.HoughLinesP(
+            edges,  # Input edge image
+            1,  # Distance resolution in pixels
+            np.pi / 180,  # Angle resolution in radians
+            threshold=200,  # Min number of votes for valid line
+            minLineLength=300,  # Min allowed length of line
+            maxLineGap=10  # Max allowed gap between line for joining them
+        )
+
+        num_lines_horizontal = 0
+        num_lines_vertical = 0
+        # Remove any lines above lowest top horizontal line (assume to be the sideline or endzone)
+        # Disregard any lines below the midpoint_y
+        if lines is None:
+            return 'none-type'
+
+        for points in lines:
+            # Extracted points nested in the list
+            x1, y1, x2, y2 = points[0]
+
+            # cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            top = y2 - y1
+            bottom = x2 - x1
+
+            if bottom == 0:
+                slope = 0.0
+            else:
+                slope = abs(top / bottom)
+
+            if slope < 0.5:
+                # Horizontal line
+                num_lines_horizontal += 1
+            else:
+                num_lines_vertical += 1
+
+        if num_lines_horizontal > num_lines_vertical:
+            return 'endzone'
+        else:
+            return 'sideline'
 
     def draw_lines(self, frame, edges):
         # Apply HoughLinesP method to directly obtain line end points
